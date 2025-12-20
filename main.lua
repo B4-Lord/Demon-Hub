b56a364e32f9e85436a58db2b2172c92d166bdab
B4 <adrianinholeao2007@gmail.com>
2025-12-20 18:05:20 -0300

    Increase main frame height to 600 pixels

--- main.lua
+++ main.lua
@@ -54,7 +54,7 @@ end
 -- HUB
 --==================================================
 local main = Instance.new("Frame", gui)
-main.Size = UDim2.new(0,500,0,300)
+main.Size = UDim2.new(0,500,0,600)
 main.Position = UDim2.new(0.5,-250,0.5,-150)
 main.BackgroundColor3 = Color3.fromRGB(18,18,18)
 main.Visible = true
